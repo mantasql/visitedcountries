7373112bae3b0e41e10f0175761bca0d00038153
Minvydas <mimciks@gmail.com>
2022-03-31 01:01:58 +0300

    button to add to array countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import DropDownCountries from "./components/DropDownCountries";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <DropDownCountries />;
+  return (
+    <div>
+      <style type="text/css">
+        {`
+          body {
+            background-color: black;
+            color: white;
+          }
+          li {
+            list-style: none;
+          }
+          `}
+      </style>
+      <DropDownCountries />
+    </div>
+  );
 }
 
 export default App;
--- src/components/DropDownCountries.js
+++ src/components/DropDownCountries.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+
+const countriesList = [];
 
 const DropDownCountries = () => {
   const [countries, setCountries] = useState([]);
+  const [selects, setSelects] = useState();
 
   const URL = "https://restcountries.com/v2/all?fields=name";
 
@@ -14,12 +19,30 @@ const DropDownCountries = () => {
   });
 
   let optionItems = countries.map((country) => (
-    <option value={country.name}>{country.name}</option>
+    <option key={country.name} value={country.name}>
+      {country.name}
+    </option>
   ));
+
+  const addCountry = () => {
+    countriesList.push(selects);
+    console.log(countriesList);
+  };
 
   return (
     <div className="allContent">
-      <select>{optionItems}</select>
+      <h1>{selects}</h1>
+      <Form.Select
+        style={{ fontSize: 15, width: 300 }}
+        value={selects}
+        onChange={(e) => setSelects(e.target.value)}
+      >
+        {optionItems}
+      </Form.Select>
+      <button onClick={addCountry}>Add Country</button>
+      {countriesList.map((country) => (
+        <li>{country}</li>
+      ))}
     </div>
   );
 };
