1d4a31465573d397e5a842d05c6c1e0ae873bb61
Minvydas <mimciks@gmail.com>
2022-04-03 16:43:16 +0300

    comitting updated addbutton with getting select

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -3,18 +3,6 @@ import React, { useEffect, useState } from "react";
 const countriesList = [];
 
 const AddButton = ({ selected, onSelect }) => {
-  /*let displayIt = filtered.map((item) => (
-    <ul key={item.name}>
-      <h2>{item.name}</h2>
-      <li>Region: {item.region}</li>
-      <li>Area: {item.area}km²</li>
-      <li>Population: {item.population}</li>
-      <li>Code: {item.alpha3Code}</li>
-    </ul>
-  ));*/
-
-  const addCountry = () => {};
-
   return (
     <div>
       <button onClick={() => onSelect(selected)}>Add Country</button>
